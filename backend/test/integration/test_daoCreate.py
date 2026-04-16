48c561302d4e93fd1395ed66c56559ff7ff347ad
Jakob <jahl24@student.bth.se>
2026-04-16 15:03:07 +0200

    add tests for the test cases for dao create

--- backend/test/integration/test_daoCreate.py
+++ backend/test/integration/test_daoCreate.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 from src.util.dao import DAO
 from src.util.validators import getValidator
+from pymongo.errors import WriteError, DuplicateKeyError
 
 
 @pytest.fixture
@@ -38,3 +39,114 @@ def daoObj(jsonFile):
     patcher.stop()
     return dao
 
+@pytest.mark.integration
+def test_create_valid_data(daoObj):
+
+    # Arrange
+    data = {
+        'firstName': 'Jakob',
+        'email': 'jakob@hotmail.com',
+        'lastName': 'Efternamn'
+    }
+
+    # Act
+    res = daoObj.create(data)
+
+    # Assert
+    assert res is not None
+    assert res['_id'] is not None
+    assert res['firstName'] == 'Jakob'
+    assert res['lastName'] == 'Efternamn'
+
+@pytest.mark.integration
+def test_create_invalid_firstname(daoObj):
+
+    # Arrange
+    data = {
+        'firstName': 1234,
+        'email': 'jakob@hotmail.com',
+        'lastName': 'Efternamn'
+    }
+
+    # Act/Assert
+    with pytest.raises(WriteError):
+        daoObj.create(data)
+
+
+@pytest.mark.integration
+def test_create_invalid_email(daoObj):
+
+    # Arrange
+    data = {
+        'firstName': 'Jakob',
+        'email': 123,
+        'lastName': 'Efternamn'
+    }
+
+    # Act/Assert
+    with pytest.raises(WriteError):
+        daoObj.create(data)
+
+@pytest.mark.integration
+def test_create_invalid_lastname(daoObj):
+
+    # Arrange
+    data = {
+        'firstName': 'Jakob',
+        'email': 'jakob@hotmail.com',
+        'lastName': 123
+    }
+
+    # Act/Assert
+    with pytest.raises(WriteError):
+        daoObj.create(data)
+
+@pytest.mark.integration
+def test_create_no_firstname(daoObj):
+
+    # Arrange
+    data = {
+        'email': 'jakob@hotmail.com',
+        'lastName': 'Efternamn'
+    }
+
+    # Act/Assert
+    with pytest.raises(WriteError):
+        daoObj.create(data)
+
+@pytest.mark.integration
+def test_create_no_email(daoObj):
+
+    # Arrange
+    data = {
+        'firstName': 'Jakob',
+        'lastName': 'Efternamn'
+    }
+
+    # Act/Assert
+    with pytest.raises(WriteError):
+        daoObj.create(data)
+
+@pytest.mark.integration
+def test_create_email_notUnique(daoObj):
+
+    # Arrange
+    data1 = {
+        'firstName': 'Jakob',
+        'email': 'jakob@hotmail.com',
+        'lastName': 'Efternamn'
+    }
+
+    data2 = {
+        'firstName': 'Jakob',
+        'email': 'jakob@hotmail.com',
+        'lastName': 'Annan'
+    }
+
+    # Act
+    # Create the first user
+    daoObj.create(data1)
+
+    # Act/Assert
+    with pytest.raises(DuplicateKeyError):
+        daoObj.create(data2)

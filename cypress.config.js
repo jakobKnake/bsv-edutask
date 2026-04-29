3f8ceccb57aed3f2f63ed6f2c1785f348b47e2a3
Ricmal08 <malmgren.kontakt@gmail.com>
2026-04-29 23:04:36 +0200

    chore: Initial setup for GUI testing

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    
+    specPattern: 'test/gui/**/*.cy.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
